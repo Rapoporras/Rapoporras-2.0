e71b0aee147cc50cf0d76dd10b095e95c925bc17
Ignacio <info@rapoporras.com>
2019-09-06 10:01:34 +0200

    Añadir reCAPTCHA al sitio web

--- contacto_Ajax.php
+++ contacto_Ajax.php
@@ -4,13 +4,31 @@ include './assets/functions/functionsAjax.php';
 
 if ($_POST["mensaje"]) {
 
-    $para = 'ignacioporras93@gmail.com';
-    $titulo = $_POST["titulo"];
-    $mensaje = $_POST["mensaje"];
-    $cabeceras = 'From: info@rapoporras.com' . "\r\n" .
+    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+    $recaptcha_secret = '6LcD_7YUAAAAALknOmQFhv8p5NeSIgeECDTnK290';
+    $recaptcha_response = $_POST['recaptcha_response'];
+    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+    $recaptcha = json_decode($recaptcha);
+
+    if ($recaptcha->score >= 0.7) {
+        // OK. ERES HUMANO, EJECUTA ESTE CÓDIGO
+        $para = 'ignacioporras93@gmail.com';
+        $titulo = $_POST["titulo"];
+        $mensaje = $_POST["mensaje"];
+        $cabeceras = 'From: info@rapoporras.com' . "\r\n" .
             'Reply-To:  info@rapoporras.com' . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
 
-    mail($para, $titulo, $mensaje, $cabeceras);
+        mail($para, $titulo, $mensaje, $cabeceras);
+    } else {
+        // KO. ERES ROBOT, EJECUTA ESTE CÓDIGO
+        $para = 'ignacioporras93@gmail.com';
+        $titulo = $_POST["titulo"];
+        $mensaje = "Robot->".$_POST["mensaje"];
+        $cabeceras = 'From: info@rapoporras.com' . "\r\n" .
+            'Reply-To:  info@rapoporras.com' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+        mail($para, $titulo, $mensaje, $cabeceras);
+    }
 }
-?>
--- header.php
+++ header.php
@@ -1,107 +1,119 @@
 <!DOCTYPE html>
 <html lang="es" class="">
 
-    <head>
+<head>
 
-        <meta name="author" content="Ignacio Porrras"/> 
+    <meta name="author" content="Ignacio Porrras" />
 
-        <meta name="languaje" content="es_ES"/> 
+    <meta name="languaje" content="es_ES" />
 
-        <meta name="description" content="Portfolio Personal Desarrollo de Software"/> 
-        <meta name="keywords" content="desarrollo de aplicaciones, desarrollo, aplicaciones, multiplataforma, programacion, programador, rapoporras"/>
+    <meta name="description" content="Portfolio Personal Desarrollo de Software" />
+    <meta name="keywords" content="desarrollo de aplicaciones, desarrollo, aplicaciones, multiplataforma, programacion, programador, rapoporras" />
 
-        <link rel="canonical" href="http://www.rapoporras.com/"/>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Portfolio Ignacio Porras Ibáñez</title>
+    <link rel="canonical" href="http://www.rapoporras.com/" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Portfolio Ignacio Porras Ibáñez</title>
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="assets/css/mdb.min.css" rel="stylesheet">
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="assets/css/mdb.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
-        <link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicons/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicons/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicons/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicons/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicons/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicons/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="assets/img/favicons/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicons/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/favicons/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicons/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
-        <link rel="manifest" href="assets/img/favicons/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="assets/img/favicons/ms-icon-144x144.png">
-        <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/favicons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/img/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="assets/img/favicons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="assets/img/favicons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116400425-1"></script>
-        <script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116400425-1"></script>
+    <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
         gtag('js', new Date());
 
         gtag('config', 'UA-116400425-1');
-        </script>
+    </script>
 
-    </head>
+    <script src='https://www.google.com/recaptcha/api.js?render=6LcD_7YUAAAAAJMxN2m_Oz50i4Oz_izApE5erPwe'></script>
+    <script>
+        grecaptcha.ready(function() {
+            grecaptcha.execute('6LcD_7YUAAAAAJMxN2m_Oz50i4Oz_izApE5erPwe', {
+                    action: 'correos'
+                })
+                .then(function(token) {
+                    var recaptchaResponse = document.getElementById('recaptchaResponse');
+                    recaptchaResponse.value = token;
+                });
+        });
+    </script>
+</head>
 
-    <body style="margin-top: 5%;">
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-dark default-color-dark fixed-top">
+<body style="margin-top: 5%;">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark default-color-dark fixed-top">
 
-                <a class="navbar-brand" href="index.php "> 
-                    <img src="assets/logo.png" height="30" alt="Logo rapoporras">
-                </a>
+            <a class="navbar-brand" href="index.php ">
+                <img src="assets/logo.png" height="30" alt="Logo rapoporras">
+            </a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 
-                <div class="navbar-collapse collapse"  id="navbarSupportedContent">
+            <div class="navbar-collapse collapse" id="navbarSupportedContent">
 
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link font-bold" style="font-size: 18px" href="index.php"><i class="fa fa-home"></i>Inicio <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link font-bold" style="font-size: 18px" href="#Idatos"><i class="fa fa-id-card"></i>Datos Y Formación</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link font-bold" style="font-size: 18px" href="#Iproyectos"><i class="fa fa-briefcase"></i>Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link font-bold" style="font-size: 18px" href="#Iexperiencia"><i class="fa fa-wrench"></i>Experiencia</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link font-bold" style="font-size: 18px" href="#Icontactos"><i class="fa fa-comments"></i>Contacto</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav ml-auto nav-flex-icons">
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-light" href="https://www.facebook.com/rapoporras"><i class="fa fa-facebook" > </i></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-light" href="https://twitter.com/rapoporras?lang=es"><i class="fa fa-twitter"> </i></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-light" href="https://plus.google.com/108983136843608667973?hl=es"><i class="fa fa-google-plus"></i></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-light" href="https://www.linkedin.com/in/ignacio-porras-iba%C3%B1ez-074530138/"><i class="fa fa-linkedin"></i></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-light" href="https://www.instagram.com/ignacio_porras/?hl=es"><i class="fa fa-instagram"></i></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link waves-effect waves-light" href="https://github.com/Rapoporras"><i class="fa fa-github-alt"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-
-
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link font-bold" style="font-size: 18px" href="index.php"><i class="fa fa-home"></i>Inicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-bold" style="font-size: 18px" href="#Idatos"><i class="fa fa-id-card"></i>Datos Y Formación</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-bold" style="font-size: 18px" href="#Iproyectos"><i class="fa fa-briefcase"></i>Proyectos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-bold" style="font-size: 18px" href="#Iexperiencia"><i class="fa fa-wrench"></i>Experiencia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-bold" style="font-size: 18px" href="#Icontactos"><i class="fa fa-comments"></i>Contacto</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto nav-flex-icons">
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light" href="https://www.facebook.com/rapoporras"><i class="fa fa-facebook"> </i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light" href="https://twitter.com/rapoporras?lang=es"><i class="fa fa-twitter"> </i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light" href="https://plus.google.com/108983136843608667973?hl=es"><i class="fa fa-google-plus"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light" href="https://www.linkedin.com/in/ignacio-porras-iba%C3%B1ez-074530138/"><i class="fa fa-linkedin"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light" href="https://www.instagram.com/ignacio_porras/?hl=es"><i class="fa fa-instagram"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light" href="https://github.com/Rapoporras"><i class="fa fa-github-alt"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
